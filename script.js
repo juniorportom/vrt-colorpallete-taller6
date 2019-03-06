fe336666b1e063c7e2444c5d84c6f393f12f90de
Reinaldo Portocarrero <juniorportom@gmail.com>
2019-03-06 11:33:50 -0500

    Se agregan cambios para cambiar texto en el textarea, ademas se habilitan los botones de generar nueva paleta y de limpiar paleta

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //import { Disposer } from "bluebird";
 
+// Variables constantes de la aplicación 
 const MAX_TONE = 359;
 const MIN_TONE = 0;
 const DISTANCE = 360 / 5;
@@ -8,6 +9,21 @@ const DISTANCE = 360 / 5;
 function randomNumber(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
+
+
+// Funcición para agregar tiempos de espera
+async function thinkTime(n) {
+    console.log('Tomando un descanso...');
+    await sleep(n);
+    console.log('N segundos después');
+}
+
+// Función para obtener la combinación RGB en hexa
+function rgbToHex(r, g, b) {
+    let hexaValue = "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    return hexaValue.toUpperCase();
+}
+
 
 // Funcion para generar la escala de tonalidad equidistante para los 5 colores complementarios
 function randomPalette(hue) {
@@ -32,11 +48,40 @@ function generateRules() {
     let colorPallete = [];
 
     for (let i = 0; i < 5; i++) {
-        let rgb = hsvToRgb(pallete[i] / MAX_TONE, saturation, value);
-        colorPallete.push(rgb);
-        console.log(rgb);
-        $("#color" + (i + 1)).css("background-color", "rgb(" + Math.round(rgb[0]) + "," + Math.round(rgb[1]) + "," + Math.round(rgb[2]) + ")");
+        let rgb_color = hsvToRgb(pallete[i] / MAX_TONE, saturation, value);
+        colorPallete.push(rgb_color);
+        //console.log(rgb);
+        $("#color" + (i + 1)).css("background-color", "rgb(" + Math.round(rgb_color[0]) + "," + Math.round(rgb_color[1]) + "," + Math.round(rgb_color[2]) + ")");
     }
+
+    $("#css-rules").text("");
+
+    var rules = ".website-background {color: " + rgbToHex(Math.round(colorPallete[0][0]), Math.round(colorPallete[0][1]), Math.round(colorPallete[0][2])) + "}\n\n" +
+        ".element-text {color: " + rgbToHex(Math.round(colorPallete[1][0]), Math.round(colorPallete[1][1]), Math.round(colorPallete[1][2])) + "}\n\n" +
+        ".element-border {color: " + rgbToHex(Math.round(colorPallete[2][0]), Math.round(colorPallete[2][1]), Math.round(colorPallete[2][2])) + "}\n\n" +
+        ".element-background {color: " + rgbToHex(Math.round(colorPallete[3][0]), Math.round(colorPallete[3][1]), Math.round(colorPallete[3][2])) + "}\n\n" +
+        ".header {color: " + rgbToHex(Math.round(colorPallete[4][0]), Math.round(colorPallete[4][1]), Math.round(colorPallete[4][2])) + "}\n";
+
+    $("#css-rules").text(rules);
+
+}
+
+// Función para devolver la paleta a los colores iniciales
+function resetPallete() {
+    for (let i = 0; i < 5; i++) {
+
+        $("#color" + (i + 1)).css("background-color", rgbToHex(255, 255, 255));
+    }
+
+    $("#css-rules").text("");
+
+    var rules = ".website-background {color: " + rgbToHex(255, 255, 255) + "}\n\n" +
+        ".element-text {color: " + rgbToHex(255, 255, 255) + "}\n\n" +
+        ".element-border {color: " + rgbToHex(255, 255, 255) + "}\n\n" +
+        ".element-background {color: " + rgbToHex(255, 255, 255) + "}\n\n" +
+        ".header {color: " + rgbToHex(255, 255, 255) + "}\n";
+
+    $("#css-rules").text(rules);
 
 }
 
