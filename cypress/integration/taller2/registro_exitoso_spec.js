796d5ecc58e5d7835f140abd2c7016776cc25e62
Reinaldo Portocarrero <juniorportom@gmail.com>
2019-03-06 13:42:55 -0500

    Se agregan scripts de cypress de taller 2, se agregan lineas de screenshots y se ejecutan los specs, capturando las imagenes correspondientes

---
+++ cypress/integration/taller2/registro_exitoso_spec.js
@@ -0,0 +1,22 @@
+describe('Los estudiantes login', function() {
+    it('Visits los estudiantes and fails at login', function() {
+        cy.visit('https://losestudiantes.co')
+        cy.contains('Cerrar').click()
+            //Lineas nuevas  
+        cy.contains('Ingresar').click()
+        cy.wait(3000)
+        cy.screenshot('1_Inicio_Registro')
+        cy.get('.cajaSignUp').find('input[name="nombre"]').click().type("Junior")
+        cy.get('.cajaSignUp').find('input[name="apellido"]').click().type("Portocarrero")
+        cy.get('.cajaSignUp').find('input[name="correo"]').click().type("prueba2@example.com")
+        cy.get('.cajaSignUp').find('select[name="idPrograma"]').select("Ingeniería de Sistemas y Computación")
+        cy.get('.cajaSignUp').find('input[name="password"]').click().type("12345678")
+        cy.get('.cajaSignUp').find('input[name="acepta"]').click()
+        cy.wait(3000)
+        cy.screenshot('2_Formulario_lleno')
+        cy.get('.cajaSignUp').contains('Registrarse').click()
+        cy.contains('Registro exitoso!')
+        cy.wait(3000)
+        cy.screenshot('3_Registro_Exitoso')
+    })
+})
